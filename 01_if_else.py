b7ae057ecc81a4d773d829d00d9fea9b0920ffa4
Rytham B. <123790286+rytham3@users.noreply.github.com>
2025-09-07 23:32:27 +0530

    Add files via upload

---
+++ 01_if_else.py
@@ -0,0 +1,8 @@
+# if else statement :
+
+a = int(input("Enter your age: "))
+
+if(a>18):
+    print("Congrats, You are eligible to Vote.")
+else:
+    print("Sorry, You arn't eligible to Vote.")
---
+++ 02_if_elif_else_ladder.py
@@ -0,0 +1,11 @@
+# if elif else ladder
+
+
+a = int(input("Enter a number: "))
+
+if(a>0):
+    print("It is a natural number.")
+elif(a==0):
+    print("It is a zero.")
+else:
+    print("It is a Negative Integer.")
